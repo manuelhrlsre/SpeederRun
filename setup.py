faa7bfa7c1c19dd66dd645573ce92784a7854e73
ManuelRHL <manuelhrl.sre@gmail.com>
2022-08-22 16:24:49 -0500

    fix: ajuste 220822 app'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from curses import window
+from tkinter import tk
+from tkinter import *
+
+from prompt_toolkit import application
+
+def Speeder():
+    def __init__():
+        pass
+
+def main(): 
+    root = tk.Tk()
+    app = Speeder(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
